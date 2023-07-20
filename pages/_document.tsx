5bcdfd85b35e00d33bed2e0658efbabb63f02970
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-07-20 20:43:32 -0300

    adicionar google search console

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-
+          <meta name="google-site-verification" content="1vj-BWIM7Q44-DS2r-c-5z1QhWBnwbKodjGeCNnQ2_0" />
           <meta name="theme-color" content="#2c74db" />
         </Head>
         <body className="scrollbar scrollbar-thumb-colorBlue scrollbar-track-gray-200 overflow-x-hidden overflow-y-auto scrollbar-hide md:scrollbar-default">
