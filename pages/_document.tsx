d39403d188b49f4155658da33cb73586a2e09339
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-07-20 20:31:15 -0300

    correção caracteres especiais

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="google-site-verification" content="rRKlKFWnlhU_kUGxe4fgENDoEIIcYSSc1DbdpRMn1kY" />
+
           <meta name="theme-color" content="#2c74db" />
         </Head>
         <body className="scrollbar scrollbar-thumb-colorBlue scrollbar-track-gray-200 overflow-x-hidden overflow-y-auto scrollbar-hide md:scrollbar-default">
