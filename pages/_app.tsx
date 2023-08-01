80c4cef3190d0f4356424d578e1f217fa112e6fd
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-07-31 21:35:59 -0300

    parallax

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-//import '../styles/tailwind.css'
-import "../styles/globals.css";
-import "tailwindcss/tailwind.css";
+import '../styles/tailwind.css'
+//import "../styles/globals.css";
+//import "tailwindcss/tailwind.css";
 import React, { useEffect } from 'react';
 import { AppProps } from 'next/app'
 import Head from 'next/head';
