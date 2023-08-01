a93366d282a492ae45bb75d24b3428e5d3668d16
next-dev-frontend <matheus.dev.frontend@gmail.com>
2023-07-31 21:26:17 -0300

    parallax

--- components/BgParallax.js
+++ components/BgParallax.js
@@ -1,21 +1,27 @@
+
 import React from 'react';
+import Image from 'next/legacy/image';
 
 function BgParallax() {
   return (
-    <div>
-      <section id="bgParallax" className="select-none relative mx-auto flex bg-center bg-fixed bg-no-repeat items-center bg-cover justify-center w-full max-w-full h-96 px-4 py-8">
-        <img loading="lazy" className="absolute w-full h-full top-0 left-0 bg-cover bg-center bg-no-repeat bg-fixed" src="/backgrounds/bg3-convertext.webp" alt="Image Background" />
-        <div className="sticky p-4 h-max max-w-3xl">
 
-          <h3 className="font-bold border-2 border-color-white text-white bg-green-700 text-center p-2 text-3xl md:text-4xl lg:text-5xl rounded">NO REGISTRATION!</h3>
-          <br />
-          <h3 className="font-bold border-2 border-color-white text-white bg-green-700 text-center p-2 text-3xl md:text-4xl lg:text-5xl rounded">UNLIMITED TEXT SIZE!!</h3>
-          <br />
-          <h3 className="font-bold border-2 border-color-white text-white bg-green-700 text-center p-2 text-3xl md:text-4xl lg:text-5xl rounded">TOTALLY FREE FOREVER!!!</h3>
-
-        </div>
-      </section>
+    <div id="bgParallax" className="select-none relative flex bg-center bg-fixed bg-no-repeat items-center bg-cover justify-center w-full h-96 px-4 py-8">
+      <Image
+        src="/backgrounds/bg3-convertext.webp"
+        alt="logo da ConverText"
+        layout="fill"
+        objectFit="cover"
+        loading='lazy'
+      />
+      <div className="sticky p-4 h-max max-w-3xl bg-transparent">
+        <h3 className="font-bold border-2 border-color-white text-white bg-green-700 text-center p-2 text-3xl md:text-4xl lg:text-5xl rounded">NO REGISTRATION!</h3>
+        <br />
+        <h3 className="font-bold border-2 border-color-white text-white bg-green-700 text-center p-2 text-3xl md:text-4xl lg:text-5xl rounded">UNLIMITED TEXT SIZE!!</h3>
+        <br />
+        <h3 className="font-bold border-2 border-color-white text-white bg-green-700 text-center p-2 text-3xl md:text-4xl lg:text-5xl rounded">TOTALLY FREE FOREVER!!!</h3>
+      </div>
     </div>
+
   );
 }
 
